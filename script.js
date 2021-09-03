1ff4ff56f65c56d68bb6f9e485d8bddb870be8e6
Andressa Collin <ancmalta@gmail.com>
2021-09-03 03:01:43 +0000

    versao de entrega - function para os alimentos com switch-case garantindo msg de erro no caso de alimento inexistente; function de calculos e msg; functions para aumentar e diminuir o tempo deaquecimento.

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
-function microondas(tempo,padrao) {
+let tempo;
+let padrao;
+let opcao;
+let msgInicio;
+let msgAcao = `Utilize aumentar() ou reduzir() para alterar o tempo em 5 segundos. Utilize ligar() para iniciar o aquecimento.`; 
+let msgTempoAtual;
+
+function updateMsgs() {
+    msgInicio = `Você escolheu a opção ${opcao}, tempo padrão ${tempo} segundos.`;
+    msgTempoAtual = `Tempo atual: ${tempo} segundos.`;
+    padrao == tempo ? console.log(msgInicio) : console.log(msgTempoAtual);
+    console.log(msgAcao);
+}
+
+function microondas(alimento) {
+    switch (alimento) {
+        case 'Pipoca':
+            padrao = 10;
+            tempo = padrao;
+            opcao = alimento;    
+            updateMsgs()        
+            break;
+        case 'Macarrão':
+        case 'Brigadeiro':
+            padrao = 8;
+            tempo = padrao;
+            opcao = alimento;
+            updateMsgs()
+            break;
+        case 'Carne':
+            padrao = 15;
+            tempo = padrao;
+            opcao = alimento;
+            updateMsgs()
+            break;
+        case 'Feijão':
+            padrao = 12;
+            tempo = padrao;
+            opcao = alimento;
+            updateMsgs()
+            break;
+        default:
+            console.log('Prato inexistente');
+            break;
+    }
+}
+
+function aumentar() {
+    tempo += 5;
+    updateMsgs()
+}
+
+function reduzir() {
+    tempo -= 5;
+    updateMsgs()
+}
+
+function ligar() {
     if (tempo < padrao) {
         msg = 'Tempo insuficiente.';
     } else if (tempo >= padrao*3) {
@@ -8,13 +65,5 @@ function microondas(tempo,padrao) {
     } else {
         msg = 'Prato pronto, bom apetite!!!'
     }
-    return msg
+    console.log(msg);
 }
-
-function pipoca(tempo) {
-    padrao = 10;
-    microondas(tempo,padrao)
-    return msg
-}
-
-console.log(pipoca(10))
